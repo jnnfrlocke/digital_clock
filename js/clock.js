22dd7451111653d4405def2f1836dba001eb50ef
Jen <jnnfrlocke@gmail.com>
2017-04-27 20:47:48 -0500

    Add sun and star to day and night themes

--- js/clock.js
+++ js/clock.js
@@ -1,14 +1,21 @@
+// Theme variable
+var themeSwitcher = 'automatic';
+
 // Theme Switcher
 $('button').click(function() {
+    themeSwitcher = 'manual';
     var thisButton = $(this);
     if ($('body').hasClass('day_theme')) {
         $('body').removeClass('day_theme');
         $('body').addClass('night_theme');
+        $("#icon").attr("src", "images/mark-as-favourite-star.png");
     } else {
         $('body').removeClass('night_theme');
         $('body').addClass('day_theme');
+        $("#icon").attr("src", "images/shining-sun.png");
     }
 })
+
 
 // Run the clock
 function clock() {
@@ -18,21 +25,21 @@ function clock() {
 
     // extract hours, display correctly
     var hours = $currentTime.getHours();
-    console.log(hours);
+    // console.log(hours);
     if (hours > 12) {
         hours -= 12;
     }
 
     // extract minutes, display correctly
     var minutes = $currentTime.getMinutes();
-    console.log(minutes);
+    // console.log(minutes);
     if (minutes < 10) {
         minutes = '0' + minutes;
     }
 
     // extract seconds, display correctly 
     var seconds = $currentTime.getSeconds();
-    console.log(seconds);
+    // console.log(seconds);
     if (seconds < 10) {
         seconds = '0' + seconds;
     }
@@ -42,7 +49,15 @@ function clock() {
     $('#minutes h1').text(minutes);
     $('#seconds h1').text(seconds);
 
+    // Automate theme based on time of day
+    if (hours > 5 && hours < 19 && themeSwitcher == 'automatic') {
+        $('body').removeClass('day_theme');
+        $('body').addClass('night_theme');
+        $("#icon").attr("src", "images/mark-as-favourite-star.png")
+    }
+
 }
+
 
 // update the time every second
 
