d980e093304320df141e7e1810bdbfda6a7a9ee4
Jen <jnnfrlocke@gmail.com>
2017-05-02 02:27:47 -0500

    Fix auto theme.

--- js/clock.js
+++ js/clock.js
@@ -1,48 +1,3 @@
-// Run the clock
-// function clock() {  ****Removed function - document.ready is deprecated
-//Get the current Date
-var $currentTime = new Date();
-// extract hours, display correctly
-var hours = $currentTime.getHours();
-// console.log(hours);
-if (hours > 12) {
-    hours -= 12;
-    // Display 12 am- 12:59 am correctly
-} else if (hours == 0) {
-    hours += 12;
-}
-// extract minutes, display correctly
-var minutes = $currentTime.getMinutes();
-// console.log(minutes);
-if (minutes < 10) {
-    minutes = '0' + minutes;
-}
-// extract seconds, display correctly 
-var seconds = $currentTime.getSeconds();
-// console.log(seconds);
-if (seconds < 10) {
-    seconds = '0' + seconds;
-}
-
-// Display Time
-$('#hours h1').text(hours);
-$('#minutes h1').text(minutes);
-$('#seconds h1').text(seconds);
-
-
-// ******THEME******
-// Automate theme based on time of day
-
-// Strict hours because in hours above, 12am (0) gets 12 added on so 12-1 am the daytime theme showed.
-// var strictHours = $currentTime.getHours();
-
-// TODO: This statement, along with lines 11-12, makes 12-1am display the daytime theme
-if (hours < 6 || hours > 19 && themeSwitcher == 'automatic') {
-    $('body').removeClass('day_theme');
-    $('body').addClass('night_theme');
-    $("#icon").attr("src", "images/mark-as-favourite-star.png")
-}
-
 // Theme variable
 var themeSwitcher = 'automatic';
 
@@ -57,19 +12,59 @@ $('button').click(function() {
     } else {
         $('body').removeClass('night_theme');
         $('body').addClass('day_theme');
-        // It takes a little while for this icon to load
         $("#icon").attr("src", "images/shining-sun.png");
     }
 })
 
-// }
 
-// FIXME: Removing this fixed the manual theme switcher, but broke the continuous clock
+// Run the clock
+function clock() {
+    //Get the current Date
+    var $currentTime = new Date();
+
+
+    // extract hours, display correctly
+    var hours = $currentTime.getHours();
+    // console.log(hours);
+    if (hours > 12) {
+        hours -= 12;
+    }
+
+    // extract minutes, display correctly
+    var minutes = $currentTime.getMinutes();
+    // console.log(minutes);
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+
+    // extract seconds, display correctly 
+    var seconds = $currentTime.getSeconds();
+    // console.log(seconds);
+    if (seconds < 10) {
+        seconds = '0' + seconds;
+    }
+
+    // Display Time
+    $('#hours h1').text(hours);
+    $('#minutes h1').text(minutes);
+    $('#seconds h1').text(seconds);
+
+    // Automate theme based on time of day
+    if (hours < 5 || hours > 19 && themeSwitcher == 'automatic') {
+        $('body').removeClass('day_theme');
+        $('body').addClass('night_theme');
+        $("#icon").attr("src", "images/mark-as-favourite-star.png")
+    }
+
+}
+
+
 // update the time every second
-// Document.ready is deprecated
-// $(document).ready(function() {
-//     clock();
-//     setInterval('clock()', 1000);
-// })
 
-//TODO: Refactor
+$(document).ready(function() {
+    clock();
+    setInterval('clock()', 1000);
+})
+
+
+//refactor
